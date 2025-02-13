fe3d2cd281f7a62e820dcf32b6bf58541c8f0450
I.Niharika <inaparthyniharika@gmail.com>
2025-02-13 15:07:50 +0530

    index.js file had been changed

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ app.use('/uploads',express.static('uploads'));
 app.listen(PORT, () => {
    console.log(`server started and running at ${PORT}`);
 } )
-app.use('/home' , (req , res)=>{
+app.use('/' , (req , res)=>{
    res.send("<h1> Welcome to GNANI")
 })
